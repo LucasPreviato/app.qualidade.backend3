7bbd60413e58162189f8f1a69ebdec091abeb009
Lucas Previato <lucaspreviato@Live.com>
2022-10-10 12:05:14 -0300

    chroe(nest/users): generate users modules

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,10 +2,6 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class User {
-  @Field(() => String, { description: 'Example field (placeholder)' })
-  id : string
-  @Field()
-  name: string
-  @Field()
-  password: string
+  @Field(() => Int, { description: 'Example field (placeholder)' })
+  exampleField: number;
 }
