92ed7ace5c83e548a36ab72f80ba96d32f4afd4c
Lucas Previato <lucaspreviato@Live.com>
2022-10-11 23:02:22 -0300

    feat(nest/auth): implement login, guard, and localStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+import { LocalStrategy } from './strategies/local-strategy';
 
 @Module({
-  providers: [AuthService]
+    imports : [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions :{expiresIn:"30d"},
+        })
+    ],
+    providers: [AuthService, LocalStrategy, JwtStrategy, AuthResolver]
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        throw new Error('Method not implemented.');
+    }
+}
---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,15 @@
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Resolver()
+export class AuthResolver {
+    constructor(private readonly authService: AuthService) {}
+
+    @Mutation(() => Login)
+    @UseGuards(LocalAuthGuard)
+    login(@Args('createUserInput') {
+    //   return  this.authService.login();
+    }
+}
