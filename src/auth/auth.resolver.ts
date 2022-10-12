2fef882207b0261899a148c29a13a3a133be8736
Lucas Previato <lucaspreviato@Live.com>
2022-10-12 00:50:01 -0300

    feat(nest/auth): implement jwt

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,15 +1,17 @@
-import { UseGuards } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Request, UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
+import { LoginUserInput } from './dto/login-user.input';
+import { UserToken } from './dto/user-token';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Resolver()
 export class AuthResolver {
     constructor(private readonly authService: AuthService) {}
 
-    // @Mutation(() => Login)
-    // @UseGuards(LocalAuthGuard)
-    // login(@Args('createUserInput') {
-    // //   return  this.authService.login();
-    // }
+    @Mutation(() => UserToken)
+    @UseGuards(LocalAuthGuard)
+    login(@Args('loginUserInput' )loginUserInput : LoginUserInput) {
+      return  this.authService.login(loginUserInput);
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from "bcrypt"
+import { LoginUserInput } from './dto/login-user.input';
+import { UserPayload } from './models/userPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './dto/user-token';
 @Injectable()
 export class AuthService {
-    constructor ( private readonly usersService : UsersService){}
-     async validateUser(email: string, password: string) {
+    constructor ( private readonly usersService : UsersService, private readonly jwtService : JwtService){}
+
+    async login({email}: LoginUserInput) : Promise<UserToken> {
+       const user = await this.usersService.findByEmail(email);
+        const {password, ...result} = user
+        const payload : UserPayload = {
+            sub : user.id,
+            email: user.email
+        };
+        const jwtToken = this.jwtService.sign(payload);
+        return {
+          access_token: jwtToken,
+          user : result
+        }
+    }
+    async validateUser(email: string, password: string) {
        const user = await this.usersService.findByEmail(email);
 
        if(user){
